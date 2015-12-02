d54340b0bb218976abf00a246d761d32f3ec278d
Matt Graham <grahmatt@umich.edu>
2015-12-02 18:44:32 -0500

    Almost completed MUSCL Half Time step

--- tunnelShock.C
+++ tunnelShock.C
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <cmath>
 #include <vector>
+#include <string>
 #include <fstream>
 #include <cstdlib>
 using namespace std;
@@ -12,18 +13,9 @@ struct grid {
     vector<vector<double> > rhoBCsLR;
     vector<vector<double> > MxBCsLR;
     vector<vector<double> > MyBCsLR;
-    vector<vector<double> > rhoBCsTB;
-    vector<vector<double> > MxBCsTB;
-    vector<vector<double> > MyBCsTB;
-    double dx;
-    double dy;
-    bool connect(4);
-    char bounds(4);
-    int xPoints;
-    int yPoints;
-};
-
-struct flux {
+    vector<vector<double> > rhoBCsBT;
+    vector<vector<double> > MxBCsBT;
+    vector<vector<double> > MyBCsBT;
     vector<vector<double> > rhoF_Plus;
     vector<vector<double> > rhoF_Minus;
     vector<vector<double> > rhoG_Plus;
@@ -36,7 +28,31 @@ struct flux {
     vector<vector<double> > MyF_Minus;
     vector<vector<double> > MyG_Plus;
     vector<vector<double> > MyG_Minus;
+    double dt;
+    double dx;
+    double dy;
+    bool connect [4];
+    string bounds [4];
+    int xPoints;
+    int yPoints;
 };
+
+// struct flux {
+//     vector<vector<double> > rhoF_Plus;
+//     vector<vector<double> > rhoF_Minus;
+//     vector<vector<double> > rhoG_Plus;
+//     vector<vector<double> > rhoG_Minus;
+//     vector<vector<double> > MxF_Plus;
+//     vector<vector<double> > MxF_Minus;
+//     vector<vector<double> > MxG_Plus;
+//     vector<vector<double> > MxG_Minus;
+//     vector<vector<double> > MyF_Plus;
+//     vector<vector<double> > MyF_Minus;
+//     vector<vector<double> > MyG_Plus;
+//     vector<vector<double> > MyG_Minus;
+// };
+
+#include "fluxLimiter.H"
 
 int main(int argc, char** argv){
 
@@ -52,13 +68,14 @@ int main(int argc, char** argv){
     double rhoShock = 2.0, rhoStart = 1.0;
 
     grid tunnel;
-    tunnel.rho[tunnelDim[0]][tunnelDim[1]];
-    tunnel.Mx[tunnelDim[0]][tunnelDim[1]];
-    tunnel.My[tunnelDim[0]][tunnelDim[1]];
+    tunnel.dt = 0.01;
     tunnel.dx = 0.1;
     tunnel.dy = 0.1;
     tunnel.xPoints = 50;
     tunnel.yPoints = 10; 
+    tunnel.rho[tunnel.xPoints][tunnel.yPoints];
+    tunnel.Mx[tunnel.xPoints][tunnel.yPoints];
+    tunnel.My[tunnel.xPoints][tunnel.yPoints];
     // BCs initialize
     for (i=0; i < 4; ++i) {
         if (i == 0) { // Left
@@ -67,7 +84,7 @@ int main(int argc, char** argv){
             tunnel.rhoBCsLR[tunnel.yPoints][2];
             tunnel.MxBCsLR[tunnel.yPoints][2];
             tunnel.MyBCsLR[tunnel.yPoints][2];
-            for (n=0; n < tunnel.yPoints, ++n) {
+            for (n=0; n < tunnel.yPoints; ++n) {
                 tunnel.rhoBCsLR[n][0] = rhoShock;
                 tunnel.MxBCsLR[n][0] = 0.0;
                 tunnel.MyBCsLR[n][0] = 0.0;
@@ -76,7 +93,7 @@ int main(int argc, char** argv){
         else if (i == 1) { // Right
             tunnel.connect[i] = true;
             tunnel.bounds[i] = "outside";
-            for (n=0; n < tunnel.yPoints, ++n) {
+            for (n=0; n < tunnel.yPoints; ++n) {
                 tunnel.rhoBCsLR[n][1] = rhoStart;
                 tunnel.MxBCsLR[n][1] = 0.0;
                 tunnel.MyBCsLR[n][1] = 0.0;
@@ -85,22 +102,22 @@ int main(int argc, char** argv){
         else if (i == 2) { // Bottom
             tunnel.connect[i] = false;
             tunnel.bounds[i] = "wall";
-            tunnel.rhoBCsTB[2][tunnel.xPoints];
-            tunnel.MxBCsTB[2][tunnel.xPoints];
-            tunnel.MyBCsTB[2][tunnel.xPoints];
-            for (n=0; n < tunnel.xPoints, ++n) {
-                tunnel.rhoBCsTB[0][n] = rhoStart;
-                tunnel.MxBCsTB[0][n] = 0.0;
-                tunnel.MyBCsTB[0][n] = 0.0;
+            tunnel.rhoBCsBT[2][tunnel.xPoints];
+            tunnel.MxBCsBT[2][tunnel.xPoints];
+            tunnel.MyBCsBT[2][tunnel.xPoints];
+            for (n=0; n < tunnel.xPoints; ++n) {
+                tunnel.rhoBCsBT[0][n] = rhoStart;
+                tunnel.MxBCsBT[0][n] = 0.0;
+                tunnel.MyBCsBT[0][n] = 0.0;
             }
         }
         else { // Top
             tunnel.connect[i] = false;
             tunnel.bounds[i] = "wall";
-            for (n=0; n < tunnel.xPoints, ++n) {
-                tunnel.rhoBCsTB[1][n] = rhoStart;
-                tunnel.MxBCsTB[1][n] = 0.0;
-                tunnel.MyBCsTB[1][n] = 0.0;
+            for (n=0; n < tunnel.xPoints; ++n) {
+                tunnel.rhoBCsBT[1][n] = rhoStart;
+                tunnel.MxBCsBT[1][n] = 0.0;
+                tunnel.MyBCsBT[1][n] = 0.0;
             }
         }
     }
@@ -114,24 +131,26 @@ int main(int argc, char** argv){
     }
 
     // Initialize flux variables
-    flux tunnelFlux;
-    tunnelFlux.rhoF_Plus[tunnelDim[0]][tunnelDim[1]];
-    tunnelFlux.rhoF_Minus[tunnelDim[0]][tunnelDim[1]];
-    tunnelFlux.rhoG_Plus[tunnelDim[0]][tunnelDim[1]];
-    tunnelFlux.rhoG_Minus[tunnelDim[0]][tunnelDim[1]];
-    tunnelFlux.MxF_Plus[tunnelDim[0]][tunnelDim[1]];
-    tunnelFlux.MxF_Minus[tunnelDim[0]][tunnelDim[1]];
-    tunnelFlux.MxG_Plus[tunnelDim[0]][tunnelDim[1]];
-    tunnelFlux.MxG_Minus[tunnelDim[0]][tunnelDim[1]];
-    tunnelFlux.MyF_Plus[tunnelDim[0]][tunnelDim[1]];
-    tunnelFlux.MyF_Minus[tunnelDim[0]][tunnelDim[1]];
-    tunnelFlux.MyG_Plus[tunnelDim[0]][tunnelDim[1]];
-    tunnelFlux.MyG_Minus[tunnelDim[0]][tunnelDim[1]];
+    // flux tunnelFlux;
+    tunnel.rhoF_Plus[tunnel.xPoints][tunnel.yPoints];
+    tunnel.rhoF_Minus[tunnel.xPoints][tunnel.yPoints];
+    tunnel.rhoG_Plus[tunnel.xPoints][tunnel.yPoints];
+    tunnel.rhoG_Minus[tunnel.xPoints][tunnel.yPoints];
+    tunnel.MxF_Plus[tunnel.xPoints][tunnel.yPoints];
+    tunnel.MxF_Minus[tunnel.xPoints][tunnel.yPoints];
+    tunnel.MxG_Plus[tunnel.xPoints][tunnel.yPoints];
+    tunnel.MxG_Minus[tunnel.xPoints][tunnel.yPoints];
+    tunnel.MyF_Plus[tunnel.xPoints][tunnel.yPoints];
+    tunnel.MyF_Minus[tunnel.xPoints][tunnel.yPoints];
+    tunnel.MyG_Plus[tunnel.xPoints][tunnel.yPoints];
+    tunnel.MyG_Minus[tunnel.xPoints][tunnel.yPoints];
 
     // MUSCL Half Time
+    grid tunnel_halfTime = tunnel;
     for (i=0; i < tunnel.yPoints; ++i) {
         for (j=0; j < tunnel.xPoints; ++j) {
-            xS_Plus = 
+            #include "halfMUSCL.H"
+            // xS_Plus = 
         }
     }
 
